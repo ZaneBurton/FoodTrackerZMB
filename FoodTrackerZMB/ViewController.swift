1b3fa7fc8838c6959005b69d178a00d82aefb106
Zane Burton <zaneburton@zanes-macbook-air.local>
2019-02-18 08:13:08 -0600

    Initial Commit

---
+++ FoodTrackerZMB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerZMB
+//
+//  Created by Zane Burton on 2/18/19.
+//  Copyright © 2019 Zane Burton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
